d42851499e4ed622e4abe32e535a1a1225b9c4b3
D.V. Colomban <17250935+dvcol@users.noreply.github.com>
2025-04-22 14:37:11 +0200

    Initial commit

---
+++ tsup.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: ['lib', '!lib/**/*.test.ts', '!lib/**/*.spec.ts'],
+  format: ['cjs', 'esm'],
+  dts: true,
+  clean: true,
+  splitting: true,
+  sourcemap: false,
+  tsconfig: 'tsconfig.lib.json',
+});
